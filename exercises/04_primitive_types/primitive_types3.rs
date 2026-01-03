41ec9abcedd8f9089be9f4f3a541e04187edaf5a
WangHunZi <suguguan@qq.com>
2026-01-04 07:16:44 +0800

    day1 and day2

---
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // TODO: Create an array called `a` with at least 100 elements in it.
+    // let a = ???
+    let a: [u32; 100] = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    ];
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+        panic!("Array not big enough, more elements needed");
+    }
+}
